a4926b92a4ba6fe61098475513883677f1182eeb
FerPascualF50 <pascualefernando@gmail.com>
2024-05-25 20:38:48 -0300

    feat: define cors

--- api/src/utils/middlewares.js
+++ api/src/utils/middlewares.js
@@ -9,8 +9,8 @@ import jwt from "jsonwebtoken";
 export const middlewares = express();
 
 middlewares.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:4000', 'http://localhost:4000/api/auth/google/callback'],
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  origin: process.env.CLIENT_URL,
+  methods: ['GET', 'POST'],
   credentials: true,
 }));
 
@@ -21,7 +21,7 @@ middlewares.use(session({
   secret: process.env.SESSION_SECRET || 'default_secret',
   resave: false,
   saveUninitialized: false,
-  cookie: {maxAge: 1000 * 60 * 60 * 24  },
+  cookie: { maxAge: 1000 * 60 * 60 * 24 },
 }));
 
 middlewares.use(passport.initialize());
@@ -32,19 +32,17 @@ middlewares.use((err, req, res, next) => {
   res.status(500).send({ code: 500, message: err.message });
 });
 
-
-
 export const authMiddleware = (req, res, next) => {
-   try {
-      if (!req.headers.authorization) throw new Error('Token inexistente');
-      const accsess_token = req.headers.authorization.split(" ").pop();
-      if (!accsess_token) throw new Error('Token inexistente');
-      const user = jwt.verify(accsess_token, process.env.JWT_SECRET);
-      req.user = user;
-      next();
-   } catch (error) {
-      res.json({ success: false, response: error.message });
-   }
+  try {
+    if (!req.headers.authorization) throw new Error('Token inexistente');
+    const accsess_token = req.headers.authorization.split(" ").pop();
+    if (!accsess_token) throw new Error('Token inexistente');
+    const user = jwt.verify(accsess_token, process.env.JWT_SECRET);
+    req.user = user;
+    next();
+  } catch (error) {
+    res.json({ success: false, response: error.message });
+  }
 };
 
 // export const unauthorizedMiddleware = async (req, res, next) => {
